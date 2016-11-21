724b801d79cede42d84bab5a907640af3bacb49c
Jon Holmberg <Jon Holmberg>
2016-11-21 00:26:48 -0800

    update 1.2

---
+++ public/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	
+	jQuery.get('/pieces', function(res){
+		
+		console.log(res);
+		
+	});
+		
+});
--- server.js
+++ server.js
@@ -1,4 +1,43 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+var config = require('./config');
+var mongoose = require('mongoose');
+
+var app = express();
+
+app.use(bodyParser.json());
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+
+var runServer = function(callback) {
+    mongoose.connect(config.DATABASE_URL, function(err) {
+        if (err && callback) {
+            return callback(err);
+        }
+
+        app.listen(config.PORT, function() {
+            console.log('Listening on localhost:' + config.PORT);
+			 if (callback) {
+                callback();
+            }
+			 // return res.status(200).json({message:'root url connected'})
+       
+        });
+    });
+};
+
+
+if (require.main === module) {
+    runServer(function(err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+};
+
+app.get('/pieces', function(req,res){
+	
+	res.status(200).json({message:'root url connected'});
+	
+});
