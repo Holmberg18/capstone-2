1c91e8e3e870159054c7f48a5ec4c3a83ac73843
holmberg18 <holmbergj25@gmail.com>
2016-12-01 03:05:53 +0000

    models

--- server.js
+++ server.js
@@ -10,25 +10,7 @@ var MongoStore = require('connect-mongo')(session);
 var app = express();
 var passport = require('passport');
 
-var bcrypt = require('bcryptjs');
 
-var UserSchema = new mongoose.Schema ({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    alias: String,
-    favorites: [{title: String, id: String}]
-});
-
-var User = mongoose.model('User', UserSchema);
-
-module.exports = User;
 
 
 app.use(bodyParser.json());
