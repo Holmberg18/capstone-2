59e58347b59b80767d742f9fe7c3a452a11d72cf
holmberg18 <holmbergj25@gmail.com>
2016-12-03 02:57:03 +0000

    finished search pieces

--- public/index.js
+++ public/index.js
@@ -7,21 +7,21 @@ var MOCK_LEGO_BUILDS = {
         {
             "id": "favorite_set_01",
             "text": "This is my biggest set yet!",
-            "friendId": "aaaaaa",
+            "username": "holmberg18",
             "friendName": "John Doe",
             "publishedAt": 1470016976609
         },
         {
             "id": "favorite_set_02",
             "text": "Here's a set that my little brother made",
-            "friendId": "aaaaaa",
+            "username": "holmberg18",
             "friendName": "John Doe",
             "publishedAt": 1470012976609
         },
         {
             "id": "favorite_set_03",
             "text": "A set I made when I was bored at work",
-            "friendId": "aaaaaa",
+            "username": "holmberg18",
             "friendName": "John Doe",
             "publishedAt": 1470011976609
         }
@@ -77,9 +77,9 @@ function displayLegoBuilds(data) {
     //     '<p>' + data.legoBuilds[index].text + '</p>');
     // }
     
-    $('body').append(
-        '<p>' + data.legoBuilds[index].text + '</p>');
-    }
+    //  $('body').append(
+    //      '<p>Here are my builds!<br><br>' + JSON.stringify(data.legoBuilds[index]) + '</p>');
+     }
 }
 
 // this function can stay the same even when we
@@ -93,6 +93,8 @@ $(function() {
 	getAndDisplayLegoBuilds();
 })
 
+$('.search-pieces').hide();
+$('.builds').hide();
 $(document).ready(function(){
 	
 	jQuery('.user-login').submit(function(e){
@@ -109,6 +111,9 @@ $(document).ready(function(){
              dataType:"json",
              success: function(data){
                console.log('Success login: '+data);
+               jQuery('.login-create').hide();
+               $('.search-pieces').show();
+                $('.builds').show();
              }
         });
 	});
@@ -127,9 +132,32 @@ $(document).ready(function(){
              dataType:"json",
              success: function(data){
                console.log('successful user registration: '+data);
+               jQuery('.login-create').hide();
+               $('.search-pieces').show();
+                $('.builds').show();
              }
         });
 	});
+
+
+	
+	jQuery('.piece-input').submit(function(e){
+	    e.preventDefault();
+	    var partId = $('#piece-number').val();
+	       $.ajax({
+             url:'https://rebrickable.com/api/get_part?key=KdPMcvXXIi&format=json&part_id='+partId,
+             type:"GET",
+             contentType:"application/json; charset=utf-8",
+             dataType:"json",
+             success: function(data){
+                console.log(data);
+               $('.piece-title').html(data.name);
+               $('.piece-description').html(data.category);
+               
+             }
+        });
+	});
+	
 	
 	
 });//end document.ready
@@ -148,10 +176,4 @@ $(document).ready(function(){
 	    
 	   //	},"json");
 	    
-	 
-// 	jQuery.get('/pieces', function(res){
-    
-// 		console.log(res);
-		
-// 	});
-		
+	
